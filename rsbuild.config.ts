f9e73095f2324c9dbf9a5bd6c0f669f57444f10c
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2024-11-05 16:22:10 +0800

    feat: add more plugins

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginVue } from '@rsbuild/plugin-vue';
+import { pluginAssetsRetry } from '@rsbuild/plugin-assets-retry';
+import { pluginCssMinimizer } from "@rsbuild/plugin-css-minimizer";
+import { pluginImageCompress } from "@rsbuild/plugin-image-compress";
 
 export default defineConfig({
-  plugins: [pluginVue()],
+  plugins: [pluginVue(), pluginAssetsRetry(), pluginCssMinimizer(), pluginImageCompress()],
 });
