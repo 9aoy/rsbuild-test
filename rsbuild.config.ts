ecf7ad9d8bfe8cd6ce81e5a7a0c84780125292ac
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2024-11-05 16:25:16 +0800

    feat: add sass plugin

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginVue } from '@rsbuild/plugin-vue';
-import { pluginAssetsRetry } from '@rsbuild/plugin-assets-retry';
+import { defineConfig } from "@rsbuild/core";
+import { pluginVue } from "@rsbuild/plugin-vue";
+import { pluginAssetsRetry } from "@rsbuild/plugin-assets-retry";
 import { pluginCssMinimizer } from "@rsbuild/plugin-css-minimizer";
 import { pluginImageCompress } from "@rsbuild/plugin-image-compress";
+import { pluginSass } from "@rsbuild/plugin-sass";
 
 export default defineConfig({
-  plugins: [pluginVue(), pluginAssetsRetry(), pluginCssMinimizer(), pluginImageCompress()],
+  plugins: [
+    pluginVue(),
+    pluginAssetsRetry(),
+    pluginCssMinimizer(),
+    pluginImageCompress(),
+    pluginSass(),
+  ],
 });
